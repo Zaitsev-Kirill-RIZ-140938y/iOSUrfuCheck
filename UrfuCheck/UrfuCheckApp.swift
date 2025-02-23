71b57d90ef231ce2a8128d1cb78f1f72de0369ff
Kirill Zaitsev <iflyzed@icloud.com>
2025-02-23 18:47:47 +0500

    Initial Commit

---
+++ UrfuCheck/UrfuCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrfuCheckApp.swift
+//  UrfuCheck
+//
+//  Created by Кирилл Зайцев on 23.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct UrfuCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
